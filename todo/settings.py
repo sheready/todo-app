d8147e3c3eb99eff73701c58d4601f5a8b1e4360
sheready <csstlw@gmail.com>
2022-03-28 02:28:13 +0300

    change setting files

--- todo/settings.py
+++ todo/settings.py
@@ -129,11 +129,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
-STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
 
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
+STATICFILES_DIRS = (BASE_DIR , 'static',)
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
