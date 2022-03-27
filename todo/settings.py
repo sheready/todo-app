546034da0b9a17b8a0f7da9ac89a2e9d23640030
sheready <csstlw@gmail.com>
2022-03-28 02:25:12 +0300

    change setting files

--- todo/settings.py
+++ todo/settings.py
@@ -127,10 +127,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
+STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
 
-
-STATICFILES_DIRS = (BASE_DIR / 'static',)
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
