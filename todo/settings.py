e2ec6ebb157ef3567eec5793301d1f849a406b3f
sheready <csstlw@gmail.com>
2022-03-28 03:13:50 +0300

    decouple the settings.py

--- todo/settings.py
+++ todo/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import dj_database_url 
+from decouple import config, Csv
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -22,12 +23,12 @@ STATIC_DIR = os.path.join(BASE_DIR, 'static')
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6%m#cgk-p6u)y!s%$pw@8$=ga4wf@)ty9g&n-4ku%+oy&((d2)'
+SECRET_KEY = SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['sheready-todo-app.herokuapp.com']
+ALLOWED_HOSTS =c onfig('ALLOWED_HOSTS', cast=Csv())
 
 
 # Application definition
@@ -84,10 +85,10 @@ WSGI_APPLICATION = 'todo.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'todo',
-        'USER': 'sheready',
-        'PASSWORD': 'password678',
-        'HOST':'127.0.0.1',
+        'NAME': config('DB_USER'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
         'PORT': '5432',
     }
 }
