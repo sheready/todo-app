202d006bfd55510b9040857640fc88fd0ead16c2
sheready <csstlw@gmail.com>
2022-03-28 03:25:25 +0300

    add static root

--- todo/settings.py
+++ todo/settings.py
@@ -134,6 +134,8 @@ PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
 STATICFILES_DIRS = (BASE_DIR , 'static')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
