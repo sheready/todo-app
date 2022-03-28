5dc36bfcea3caa8d2b7cefacb18b1eef209830ad
sheready <csstlw@gmail.com>
2022-03-28 08:20:02 +0300

    remove gitignore

--- todo/settings.py
+++ todo/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
         'USER': config('DB_USER'),
         'PASSWORD': config('DB_PASSWORD'),
         'HOST': config('DB_HOST'),
-        'PORT': '',
+        'PORT': ' ',
     }
 }
 
