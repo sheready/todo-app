15829cd432d7e86a2831df92bc8ad1c4c1a3e0a0
sheready <csstlw@gmail.com>
2022-05-16 21:37:20 +0300

    edit form fields

--- users/forms.py
+++ users/forms.py
@@ -4,23 +4,24 @@ from django.contrib.auth.forms import UserCreationForm
 from users.models import Profile
 
 class UserRegisterForm(UserCreationForm):
-    first_name = forms.CharField(max_length=30, required=False, help_text='Optional')
-    last_name = forms.CharField(max_length=30, required=False, help_text='Optional')
     email = forms.EmailField(max_length=254, help_text='Enter a valid email address')
+    
 
     class Meta:
         model = User
-        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2', )
+        fields = ('username', 'email','password1', 'password2', )
+
 
 class UserUpdateForm(forms.ModelForm):
     email = forms.EmailField()
 
     class Meta:
         model = User
-        fields = ['username', 'email']
+        fields = ['username','email']
 
 class ProfileUpdateForm(forms.ModelForm):
-  
+    
+
     class Meta: 
         model = Profile
         fields = ['image', 'bio']
