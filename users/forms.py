23433c2f6f6a9ef9ec7161db3cf4e909cd2fc4b7
sheready <csstlw@gmail.com>
2022-05-19 23:56:11 +0300

    extend profile model to forms

--- users/forms.py
+++ users/forms.py
@@ -1,22 +1,23 @@
 from django import forms
-from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
-from users.models import Profile
+from django.contrib.auth.forms import UserCreationForm as BaseUserCreationForm
+from users.models import Person, Profile
+from django.contrib.auth import get_user_model
+User = get_user_model()
 
-class UserRegisterForm(UserCreationForm):
+class UserRegisterForm(BaseUserCreationForm):
     email = forms.EmailField(max_length=254, help_text='Enter a valid email address')
-    
+    phone = forms.CharField(max_length=20, required=True, help_text='Phone number that will receive OTP')
 
     class Meta:
-        model = User
-        fields = ('username', 'email','password1', 'password2', )
+        model = Person
+        fields = ('username', 'email','phone','password1', 'password2', )
 
 
 class UserUpdateForm(forms.ModelForm):
     email = forms.EmailField()
 
     class Meta:
-        model = User
+        model = Person
         fields = ['username','email']
 
 class ProfileUpdateForm(forms.ModelForm):
@@ -29,5 +30,7 @@ class ProfileUpdateForm(forms.ModelForm):
 class LoginForm(forms.Form):
     username = forms.CharField(max_length=63)
     password = forms.CharField(max_length=63, widget=forms.PasswordInput)
+    phone = forms.CharField(max_length=20, required=True, help_text='Phone number that will receive OTP')
 
-   
+class VerifyForm(forms.Form):
+    code = forms.CharField(max_length=8, required=True, help_text='Enter Code')
