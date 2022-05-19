047b1fcc8f5b7077c62d2fbc7b5cdf3cadbd59cb
sheready <csstlw@gmail.com>
2022-05-19 23:52:58 +0300

    add the verify otp url

--- users/urls.py
+++ users/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth.decorators import login_required
-from users.views import SignUpView, LoginPageView, log_out,profile,ProfileView
-
+from users.views import SignUpView, LoginPageView, log_out,profile,ProfileView, verify_code
+from . import views
 app_name = 'user'
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name="signup"),
@@ -11,6 +11,7 @@ urlpatterns = [
     path('profile_update/', profile, name= 'profile-update'),
     path('profile/', login_required(ProfileView.as_view()), name="profile"),
     path('logout/', log_out, name="logout"),
+    path('verify/', views.verify_code),
    
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
