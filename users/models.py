24f4e1682a0fde32f9ad6d84c4677b57d8786277
sheready <csstlw@gmail.com>
2022-05-19 23:54:54 +0300

    change user model to person model and add extend user model

--- users/models.py
+++ users/models.py
@@ -1,10 +1,14 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, AbstractUser
 from PIL import Image
 # Create your models here.
 
+class Person(AbstractUser):
+    phone =models.TextField(max_length=20, blank=False)
+    is_verified = models.BooleanField(default=False)
+
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(Person, on_delete=models.CASCADE)
     bio = models.TextField()
     image = models.ImageField(default='images/Avatar.png', upload_to='images')
     
